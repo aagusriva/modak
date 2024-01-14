815d470eca3dcaad8a35bf963c1d919e114d97db
aagusriva <aagusriva@gmail.com>
2024-01-14 12:50:41 -0300

    adding axios and fetching request

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 
 const HomeScreen = () => {
   const {t} = useTranslation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const resp = await axios({
+        url: 'https://api.artic.edu/api/v1/artworks',
+        method: 'GET',
+        params: {
+          page: 1,
+          limit: 1,
+        },
+      });
+      console.log(resp.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  if (loading) return <ActivityIndicator />;
+
   return (
     <View>
       <Text>{t('home.title')}</Text>
