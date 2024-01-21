2691e4f9fd1b8e6147b705d7a487cac774461e13
aagusriva <aagusriva@gmail.com>
2024-01-21 16:48:36 -0300

    configuring testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '\\.(png|jpg|jpeg|gif|svg)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: ['node_modules/(?!@rneui/themed)/'],
+  setupFiles: ['./jestSetup.js'],
 };
